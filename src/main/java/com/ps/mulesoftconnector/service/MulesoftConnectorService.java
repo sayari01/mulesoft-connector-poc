abad8b506135855c5f0203c0d957469e253f3745
sayadhik <sayadhik@WKWIN4785573.mshome.net>
2022-03-08 13:55:20 +0530

    commited code for service

--- src/main/java/com/ps/mulesoftconnector/service/MulesoftConnectorService.java
+++ src/main/java/com/ps/mulesoftconnector/service/MulesoftConnectorService.java
@@ -1,7 +1,10 @@
 package com.ps.mulesoftconnector.service;
 
+import org.springframework.stereotype.Service;
+
 import com.ps.mulesoftconnector.model.MulesoftConnectorRequest;
 
+@Service
 public interface MulesoftConnectorService {
 
 	public String getApiDetails(MulesoftConnectorRequest request) throws Exception;
